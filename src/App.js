1056f3419eb0897fede9ad4559b7c74320e057c8
niiquash <qua16006@byui.edu>
2023-03-13 16:14:44 -0600

    created and styled to do app container, input field for todo and add button.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { AiOutlinePlus } from 'react-icons/ai'
+import Todo from "./Todo"
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100 sm:mt-0`,
+  count: `text-center p-2`
+
+}
 
 function App() {
+
+  const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode'])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
+      </div>
     </div>
   );
 }
