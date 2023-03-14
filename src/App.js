9357c3a6dd8d87107c568102f0a87eed59bc5084
niiquash <qua16006@byui.edu>
2023-03-13 22:36:31 -0600

    connected frontend to backend: firebase. read, update, create functionality added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { AiOutlinePlus } from 'react-icons/ai'
 import Todo from "./Todo"
+import { db } from './firebase'
+import { query, collection, onSnapshot, updateDoc, doc, addDoc } from 'firebase/firestore'
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
@@ -15,22 +17,61 @@ const style = {
 
 function App() {
 
-  const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode'])
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
+
+  // create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e)
+    if (input === '') {
+      alert('Please enter a todo task')
+      return
+    }
+
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false
+    })
+    setInput('')
+  }
+
+  // read todo from firebase
+  useEffect(() => {
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = []
+      querySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id })
+      })
+      setTodos(todosArr)
+    })
+    return () => unsubscribe
+  }, [])
+
+  // update todo in firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed
+    })
+  }
+
+  // delete todo from firebase
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
-        <form className={style.form}>
-          <input className={style.input} type="text" placeholder="Add Todo" />
+        <form onSubmit={createTodo} className={style.form}>
+          <input value={input} onChange={(e) => setInput(e.target.value)} className={style.input} type="text" placeholder="Add Todo" />
           <button className={style.button}><AiOutlinePlus size={30} /></button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} />
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
           ))}
         </ul>
-        <p className={style.count}>You have 2 todos</p>
+        {todos.length ? <p className={style.count}>{`You have ${todos.length} todos`}</p> : null}
+
       </div>
     </div>
   );
